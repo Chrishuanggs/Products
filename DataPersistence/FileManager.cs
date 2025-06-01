17bc1859331ea6093a4259604db7c4939254189b
CHRIS ANDY HUANG RAMIREZ <chuangr@ucenfotec.ac.cr>
2025-06-01 15:30:09 -0600

    Implementacion de file manager

--- DataPersistence/FileManager.cs
+++ DataPersistence/FileManager.cs
@@ -9,12 +9,31 @@ namespace DataPersistence
 {
     public class FileManager
     {
+        private string _filePath = "products.txt";
         //Metodo que retorna una lista de productos tomada del archivo de texto.
         public List<Product> ReadProducts()
         {
             // Desarrollarlo yo
             //Leer la lista de productos de un txt y cargarlo en una lista
             var products = new List<Product>();
+
+            if (File.Exists(_filePath))
+            {
+                var lines = File.ReadAllLines(_filePath);
+
+                foreach (var line in lines)
+                {
+                    var parts=line.Split(',');
+                    var product= new Product();
+                    product.Id =Int32.Parse(parts[0]);
+                    product.Name= parts[1];
+                    product.Price=Double.Parse(parts[2]);
+                    product.Stock=Int32.Parse(parts[3]);
+
+                    products.Add(product);
+                }
+            }
+
             return products;
 
         }
@@ -22,6 +41,10 @@ namespace DataPersistence
         public void SaveProduct(Product product)
         {
             //Guardar el producto en un txt 
+            var line = product.Id + "," + product.Name + "," + product.Price + "," + product.Stock;
+
+            File.AppendAllText(line, _filePath);
+
         }
     }
 }

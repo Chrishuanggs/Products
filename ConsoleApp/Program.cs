e86558e25a10a716b69d973aee3e0fbb5c75355e
CHRIS ANDY HUANG RAMIREZ <chuangr@ucenfotec.ac.cr>
2025-06-01 15:00:28 -0600

    Estructura general y clases

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenido al gestor de productos Dummy");
+        Console.WriteLine("Digite la opcion deseada: ");
+        Console.WriteLine("1. Agregar un producto");
+        Console.WriteLine("2. Consultar listado de productos");
+
+        var option =Int32.Parse(Console.ReadLine());
+
+        switch (option)
+        {
+            case 1:
+                Console.WriteLine("Digite la informacion del producto a continuacion");
+                break;
+
+            case 2:
+                Console.WriteLine("Listado de productos existentes");
+                break;
+            case 9:
+                Console.WriteLine("Saliendo.......");
+                break;
+            default:
+                Console.WriteLine("Opcion Incorrecta");
+                break;
+        }
+
+    }
+}
---
+++ DataPersistence/FileManager.cs
@@ -0,0 +1,27 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPersistence
+{
+    public class FileManager
+    {
+        //Metodo que retorna una lista de productos tomada del archivo de texto.
+        public List<Product> ReadProducts()
+        {
+            // Desarrollarlo yo
+            //Leer la lista de productos de un txt y cargarlo en una lista
+            var products = new List<Product>();
+            return products;
+
+        }
+
+        public void SaveProduct(Product product)
+        {
+            //Guardar el producto en un txt 
+        }
+    }
+}
